cb9bf3d62ede735ebd9d3220ce981c72c8b874ef
Nikhitha-hegde <nikhitha.hegde24@gmail.com>
2025-08-14 00:59:59 +0530

    error-handling

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,17 +4,17 @@ const API_URL = import.meta.env.VITE_API_URL; // matches your backend
 
 export const getChats = async () => {
   console.log("API_URL:", API_URL);
-  const res = await axios.get(`${API_URL}/messages`);
+  const res = await axios.get(`${API_URL}/api/messages`);
   return res.data;
 };
 
 export const getMessagesByWaId = async (wa_id) => {
-  const res = await axios.get(`${API_URL}/messages/${wa_id}`);
+  const res = await axios.get(`${API_URL}/api/messages/${wa_id}`);
   return res.data;
 };
 
 export const sendMessage = async (data) => {
-  const res = await axios.post(`${API_URL}/messages`, data, {
+  const res = await axios.post(`${API_URL}/api/messages`, data, {
     headers: { "Content-Type": "application/json" }
   });
   return res.data;
