c736ef96c0d899fbb61b7e59c45ded669b77ecb1
Nikhitha-hegde <nikhitha.hegde24@gmail.com>
2025-08-14 01:10:02 +0530

    api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL; // matches your backend
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getChats = async () => {
   console.log("API_URL:", API_URL);
